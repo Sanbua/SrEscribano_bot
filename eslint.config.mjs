5175797e2107f97f972a886a2e376a5b038fa2e4
Sanbua <sandraruperez89@gmail.com>
2025-08-18 00:46:26 +0200

    Refactor ESLint configuration and improve message handling in bot

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,13 @@ export default antfu(
     stylistic: {
       indent: 2,
       quotes: 'single',
-      braceStyle: ['1tbs', { allowSingleLine: true }],
+      braceStyle: '1tbs',
     },
   },
   {
     files: ['**/*.js', '**/*.ts'],
     rules: {
+      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
       'node/prefer-global/process': 'off',
       'no-console': 'off',
       'antfu/no-top-level-await': 'off',
--- src/bot.ts
+++ src/bot.ts
@@ -26,11 +26,9 @@ bot.on('message', async (context) => {
       const timeGame = context.text?.match(regexTime)?.[0] || '-:--'
       getData(gameActual, userActual, timeGame)
       await context.send(`Vale ${context.from.firstName}, el ${gameActual?.name} del día ${getToday()} esta apuntado con un tiempo de ${timeGame}`)
-    }
-    else if (context.text?.toLowerCase().includes('moa')) {
+    } else if (context.text?.toLowerCase().includes('moa')) {
       await context.send('MOA 😊')
-    }
-    else {
+    } else {
       await context.sendAnimation('https://c.tenor.com/PQjmqBZ7TVoAAAAd/tenor.gif', {
         caption: 'Esto no es un juego 😡',
       })
