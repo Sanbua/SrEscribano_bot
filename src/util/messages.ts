98af9cf43bda2b747fa384cbbda13062666ea1cb
Sanbua <sandraruperez89@gmail.com>
2025-12-09 18:17:57 +0100

    Add message in a file

---
+++ src/util/messages.ts
@@ -0,0 +1,17 @@
+export const MESSAGES = {
+  GAME_OK: (
+    userName: string,
+    gameName: string,
+    today: string,
+    timeGame: string,
+  ) =>
+    `Vale ${userName}, el ${gameName} del día ${today} esta apuntado con un tiempo de ${timeGame}`,
+
+  MOA: 'MOA 😊',
+
+  MSG_ERROR: {
+    animation: 'https://c.tenor.com/PQjmqBZ7TVoAAAAd/tenor.gif',
+    caption: 'Esto no es un juego 😡',
+  },
+
+}
